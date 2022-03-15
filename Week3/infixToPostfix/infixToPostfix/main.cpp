2685b43fabd36f5b8c5cdd89962f9b07fbeb766c
YonghoonChung <yonghoon1999@gmail.com>
2022-03-15 21:32:42 +0900

    edit

--- Week3/infixToPostfix/infixToPostfix/main.cpp
+++ Week3/infixToPostfix/infixToPostfix/main.cpp
@@ -37,7 +37,7 @@ bool op_stack::empty(){
     return (top == 0);
 }
 char op_stack::top_element(){
-    return (s[top-1]);
+    return s[top-1];
 }
 
 bool is_operand(char ch){
@@ -50,45 +50,62 @@ bool is_operand(char ch){
         return true; // 숫자라면
 }
 int get_precedence(char op){
-    if((op=='$')||(op==')'))
-        return 0;
-    else if((op=='+')||(op=='-'))
-        return 1;
-    if((op=='*')||(op=='/')||(op=='%'))
-        return 2;
-    return 0;
+    switch (op) {
+        case '$':
+        case ')':
+            return 0;
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+        case '%':
+            return 2;
+        default:
+            return -1;
+    }
 }
-int main(int argc, const char * argv[]) {
-    string input, output;
-    op_stack stack1;
-    cout<<"Input an infix expression to convert: ";
-    cin>>input;
-    int len = (int)input.size();
+string infixToPostfix(string input){
+    op_stack stack;
+    string output;
     input += EOS;
+    int len = (int)input.size();
     
     for(int i=0; i< len;i++){
         if(is_operand(input[i])){ // 숫자라면
             output += input[i];
         }
+        else if(input[i] == '('){ // 여는 소괄호
+                stack.push(input[i]);
+        }
+        else if(input[i] == ')'){ // 닫는 소괄호
+            while(stack.top_element() != '('){
+                output += stack.top_element();
+                stack.pop();
+            }
+            stack.pop();
+        }
         else{
-            if(input[i] == '('){ // 여는 소괄호
-                stack1.push(input[i]);
+            while(!stack.empty()&& get_precedence(input[i])<=get_precedence(stack.top_element())){
+                output += stack.top_element();
+                stack.pop();
             }
-            else if(input[i] != ')'){ // 닫는 소괄호
-                while(stack1.top_element() == '('){
-                    output += stack1.pop();
-                }
-                stack1.pop();
-            }else{
-                while(!stack1.empty()&& get_precedence(input[i])<=get_precedence(stack1.top_element())){
-                    output += stack1.pop();
-                }
-            }
+            stack.push(input[i]);
         }
     }
-    while(!stack1.empty()){
-        output += stack1.pop();
+    while(!stack.empty()){
+        output += stack.top_element();
+        stack.pop();
     }
-    cout << output;
+    
+    return output;
+}
+int main(int argc, const char * argv[]) {
+    string input, output;
+    cout<<"Input an infix expression to convert: ";
+    cin>>input;
+    output = infixToPostfix(input);
+   
+    cout << output<<"\n";
     return 0;
 }
